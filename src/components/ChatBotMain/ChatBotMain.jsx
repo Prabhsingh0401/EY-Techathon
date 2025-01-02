69260c658a2eff268affc4fe8ae540511fbb7cf1
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2025-01-02 10:23:01 +0530

    Update ChatBotMain.jsx

--- src/components/ChatBotMain/ChatBotMain.jsx
+++ src/components/ChatBotMain/ChatBotMain.jsx
@@ -7,14 +7,14 @@ const ChatBotMain = () => {
         <NavBar></NavBar>
         <div className="fixed top-[15%] left-9 w-[60vw] z-50 backdrop-blur-sm rounded-lg p-4 h-[83vh] overflow-hidden">         
         <div className="flex">
-        <h1 className="font-bold text-7xl bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent">
+        <h1 className="font-bold text-7xl bg-gradient-to-r from-green-500 to-green-300 bg-clip-text text-transparent">
             SamvaadAI
         </h1>   
                 <img className="w-[6vw] rounded-[50%] ml-3" src="/ChatBotImg.gif"></img>
             </div>
             <p className="w-[80%] text-pretty mt-10 text-2xl">A multilingual chatbot that currently supports Hindi with speech-to-text functionality, and it aims to expand its language capabilities in the future. 
-                <br></br><br></br>You can ask it for financial knowledge, but since it's powered by AI, it’s important not to rely on it fully for financial decisions.</p>
-        </div>
+                <br></br><br></br>
+                One stop to get all your <span className="bg-gradient-to-r from-green-500 to-green-300 bg-clip-text text-transparent">Financial knowledge.</span></p></div>
         <ChatBot></ChatBot>
         </>
     )
